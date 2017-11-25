def7dd29edcb7666697e7807fb0d75b59ad33818
eth4kr <eth4kr@gmail.com>
2017-11-25 15:24:15 +0530

    v2

--- Projects/codedi/public/script.js
+++ Projects/codedi/public/script.js
@@ -1,48 +1,94 @@
-var socket = '';
+var socket = io();
+$(".togglebutton").hide();
+var code = "";
+var temp = "";
+var url_string = window.location.href;
+var url = new URL(url_string);
+var c = url.searchParams.get("joinedTo");
 
-socket = io();
 var isConnected = false;
+var isHost = true;
+
 socket.on("connect", function() {
 	isConnected = true;
+	$("#alias_host").html("<font color=green>Host</font>");
 })
 socket.on('disconnect', function() {
 	isConnected = false;
+	$("#alias_host").html("<font color=red>Disconnected</font>");
 });
+socket.on("dc", function() {
+	if(isHost)
+		$("#alias_client").html("<font color=red>Client (Disconnected)</font>");
+	else
+		$("#alias_client").html("<font color=red>Host (Disconnected)</font>");
+})
 socket.on('id', function(data){
-	dicode.user_connect("eth4kr", data);	
-	console.log("Connected with ID: "+data);
+	if(c) {
+		$("#alias_host").html("Client");
+		isHost = false;
+		socket.emit('joinMeTo', c);
+		dicode.user_connect("client", data);
+		$("#alias_client").html("<font color=green>Host (Connected)</font>");
+	}else { 
+		dicode.user_connect("host", data);
+		
+		console.log("Connected with ID: "+data);
+		$("#join_link").html("Link:, "+"http://127.0.0.1:8080/join/"+data);
+	}
 });
 
-var temp_text = '';
-document.onkeypress = function(e) {
+document.onkeypress = function(e){
+	if(e.ctrlKey) return;
+	var focused = document.activeElement;
 	if(!isConnected) return;
-	var user_1 = document.getElementById('user_one_editor');
-	if(e.charCode >= 34) {
-		if(user_1.value == "") {
+	var host = document.getElementById('host_editor');
+	var message_box = $("#msg_box");
+	if(focused != host || focused != message_box) return true;
+	if(e.keyCode == 13 && focused == message_box) {
+		dicode.emit_message();
+	}
+	if(e.keyCode == 9) { //tab
+		host.value += "      ";
+		$("#host_editor").focus();
+	}
+	if(e.charCode >= 30) {
+		if(host.value == "") {
 			dicode.emit_command('clear_all');
-		} else
-		dicode.emit_text(e.key)
+		}
 	}
 	if(e.keyCode == 8) {
-		dicode.emit_command('backspace')
+		dicode.emit_command('backspace');
 	}
-} 
+}
+setInterval(function() {
+	if(temp != code) {
+		console.log("changed");
+		console.log(temp)
+		temp = code;
+		console.log(temp)
+	}
+}, 200);
 socket.on('cmd', function(data) {
 	console.log(data);
-	var user_2 = document.getElementById('user_two_editor');
+	var client = document.getElementById('client_editor');
 	switch(data.text) {
 		case 'backspace':
-			user_2.value = user_2.value.slice(0, -1);
+			client.value = client.value.slice(0, -1);
 			break;
 		case 'clear_all':
-			user_2.value = "";
+			client.value = "";
+			break;
 	}
+});
+socket.on('user_joined', function(data) {
+	$("#alias_client").html('<font color=green>Client (Connected)</font>');
 })
 socket.on('text', function(data) {
 	console.log(data)
-	//if(data.username == dicode.username){
-		document.getElementById('user_two_editor').value += data.text;
-	//}
+	if(data.username != dicode.username){
+		document.getElementById('client_editor').value = data.text;
+	}
 })
 
 var dicode = {
@@ -63,7 +109,13 @@ var dicode = {
 		json.username = this.username;
 		json.id = this.id;
 		json.text = text;
-
 		socket.emit('text', json);
+	},
+	emit_message: function() {
+		var msg = $("#message_box").val();
+		var json = {};
+		json.username = this.username;
+		json.id = this.id;
+		json.text = msg;
 	}
 }
--- Projects/codedi/server.js
+++ Projects/codedi/server.js
@@ -1,24 +1,48 @@
 console.log("\033c");
-const express = require('express');
-const socketIO = require('socket.io');
-const path = require('path');
-const PORT = process.env.PORT || 8080;
-const server = express()
-  .use('/', express.static(path.join(__dirname, 'public')))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+var port = process.argv[2];
 
-const io = socketIO(server);
+var express = require('express');
+var app = express();
+var path = require('path');
+var io = require('socket.io')(app.listen(port));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+})
+.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/join/:userID', function(req, res) {
+	console.log(req.params.userID);
+	io.to(req.param('userID')).emit('user_joined');
+	res.redirect('http://127.0.0.1:8080/index.html?joinedTo='+req.param('userID'));
+});
+
+console.log('Listening on port ' + port);
 
 io.on('connection', function(client) {
-	console.log(client.id);
 	client.emit('id', client.id);
+	var config = {};
+	config.id = client.id;
+	config.inGroup = client.id;
+	console.log(client.id+' joined');
+	client.on('joinMeTo', function(data) {
+		console.log("Joining "+client.id+" in  ["+data+"]");
+		config.inGroup = data;
+		client.join(data);
+		client.broadcast.to(data).emit('user_joined')
+	});
+	client.on('disconnect', function() {
+		console.log("One user left the group: "+config.inGroup)
+		client.broadcast.to(config.inGroup).emit('dc');
+	})
+	client.join(client.id);
+	
 	client.on('text', function(data) {
-		console.log(data.text)
-		io.emit('text', data);
+		console.log("EMITing in: "+config.inGroup);
+		io.to(config.inGroup).emit('text', data);
 	});
 	client.on('cmd', function(data) {
-		console.log(data.text)
-		io.emit('cmd', data);
+		console.log(JSON.stringify(data));
+		io.to(client.inGroup).emit('cmd', data);
 	});
-	
 });
